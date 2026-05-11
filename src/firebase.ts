4e53e7fd9c5f47e74220828c33c6daaaa6ecf855
Dhiwakar <dhiwakardhiwakar83@gmail.com>
2026-05-11 10:17:45 +0530

    Updated project files

---
+++ src/firebase.ts
@@ -0,0 +1,73 @@
+import { initializeApp } from 'firebase/app';
+
+import {
+  getMessaging,
+  getToken,
+  onMessage
+} from 'firebase/messaging';
+
+const firebaseConfig = {
+
+  apiKey:
+    import.meta.env.VITE_FIREBASE_API_KEY,
+
+  authDomain:
+    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+
+  projectId:
+    import.meta.env.VITE_FIREBASE_PROJECT_ID,
+
+  storageBucket:
+    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+
+  messagingSenderId:
+    import.meta.env
+      .VITE_FIREBASE_MESSAGING_SENDER_ID,
+
+  appId:
+    import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+// INITIALIZE APP
+const app = initializeApp(firebaseConfig);
+
+// EXPORT MESSAGING
+export const messaging =
+  getMessaging(app);
+
+// REQUEST PERMISSION
+export const requestNotificationPermission =
+  async () => {
+
+    try {
+
+      const permission =
+        await Notification.requestPermission();
+
+      if (permission !== 'granted') {
+
+        return null;
+      }
+
+      const token =
+        await getToken(
+          messaging,
+          {
+            vapidKey:
+              import.meta.env
+                .VITE_FIREBASE_VAPID_KEY
+          }
+        );
+
+      return token;
+
+    } catch (error) {
+
+      console.error(error);
+
+      return null;
+    }
+  };
+
+// EXPORT onMessage
+export { onMessage };
