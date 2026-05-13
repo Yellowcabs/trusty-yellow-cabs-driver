85d6f453399fade060719464f06e1bdbb4a0ad05
Dhiwakar <dhiwakardhiwakar83@gmail.com>
2026-05-14 01:00:23 +0530

    Full project update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,17 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.trustycab.driver',
   appName: 'Trusty Cab',
   webDir: 'dist',
-  server: {
-    androidScheme: 'https',
-    allowNavigation: [
-      'ais-dev-uqgore4bofclvpax7gqjqi-242082848033.asia-southeast1.run.app',
-      'ais-pre-uqgore4bofclvpax7gqjqi-242082848033.asia-southeast1.run.app'
-    ],
-    cleartext: true
-  },
-  android: {
-    allowMixedContent: true
-  },
   plugins: {
     CapacitorHttp: {
       enabled: true,
