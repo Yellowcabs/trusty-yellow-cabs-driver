329db7040241da9685a79be2f53a42d7de4e2496
Dhiwakar <dhiwakardhiwakar83@gmail.com>
2026-05-15 00:24:35 +0530

    Full project update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.trustycab.driver',
   appName: 'Trusty Cab',
+
   webDir: 'dist',
 
-  android: {
-    allowMixedContent: true
+  server: {
+    androidScheme: 'https',
+    url: 'https://trusty-yellow-cabs-driver.vercel.app',
+    cleartext: false,
   },
 
   plugins: {
